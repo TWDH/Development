cef0088b09ce886656189f9c7cf89d17307d02f3
hezhukevin <48498805+TWDH@users.noreply.github.com>
2021-08-29 22:59:41 -0400

    log

--- SpringBoot/雷神/02.代码/springboot_dev/src/main/java/com/hezhu/ottdev/config/logbookConfig.java
+++ SpringBoot/雷神/02.代码/springboot_dev/src/main/java/com/hezhu/ottdev/config/logbookConfig.java
@@ -3,11 +3,16 @@ package com.hezhu.ottdev.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.logbook.*;
+import static org.zalando.logbook.json.JsonPathBodyFilters.jsonPath;
+import static java.util.regex.Pattern.compile;
 
 import static org.zalando.logbook.Conditions.contentType;
 
 @Configuration
 public class logbookConfig {
+
+
+
     @Bean
     public RequestFilter requestFilter() {
         System.out.println(" === This is requestFilter === ");
@@ -16,14 +21,18 @@ public class logbookConfig {
             @Override
             public String replace(HttpRequest message) {
 
+                String host = message.getHost();
                 System.out.println(message);
-                return "This is Replace";
+                return host;
             }
         });
     }
 
-    /*@Bean
+    @Bean
     public BodyFilter bodyFilter() {
+        BodyFilter jsonFilter = jsonPath("$.active").replace("unknown");
 
-    }*/
+        BodyFilter merge = BodyFilter.merge(BodyFilters.defaultValue(), BodyFilters.truncate(1));
+        return jsonFilter;
+    }
 }
