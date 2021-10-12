a776a00b28bdd36e44afbfded02fcdeea4efefe9
Kevin Zhu <48498805+TWDH@users.noreply.github.com>
2021-10-12 09:20:46 -0400

    jpa

---
+++ 11.SpringDataJpa/2.Code/src/main/java/com/ott/hezhu/hibernatejpa/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.ott.hezhu.hibernatejpa.repository;
+
+import com.ott.hezhu.hibernatejpa.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @Author He Zhu
+ * @Date 2021-10-05 12:18 p.m.
+ * @Version 1.0
+ */
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
