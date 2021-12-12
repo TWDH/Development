b063b1d0711a9c6830f0ca4183ca4299379e3669
hezhukevin <48498805+TWDH@users.noreply.github.com>
2021-12-12 13:49:53 -0500

    leetcode

--- 00.General/JavaExercise/src/sortExercise.java
+++ 00.General/JavaExercise/src/sortExercise.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class sortExercise {
     public static void main(String[] args) {
-        int[] A = new int[]{3, 2, 4, 1};
+        int[] A = new int[]{3, 2, 4, 1}; 
         Integer[] B = new Integer[]{1,2,3,4};
         Arrays.sort(B, (i, j) -> A[j - 1] - A[i - 1]);
 
