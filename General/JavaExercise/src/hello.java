b724a438f73447a76450a8a85867fd2a141c2b84
He Zhu <48498805+TWDH@users.noreply.github.com>
2022-01-20 18:50:04 -0500

    LeetCode 1492

--- General/JavaExercise/src/hello.java
+++ General/JavaExercise/src/hello.java
@@ -1,12 +1,14 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
-public class Solution {
-
+/**
+ * @Author He Zhu
+ * @Date 2022-01-20 11:05 a.m.
+ * @Version 1.0
+ */
+public class hello {
     public static void main(String[] args) {
         int i;
         for( i = 0; i <= 5; i++){
             System.out.println("i: " + i);
+            // 相当于这在里 i++
         }
 
         System.out.println("=========================");
